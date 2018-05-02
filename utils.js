0ab402c14fb024ad4ae30511bd21a73edc7a9cf6
lipenghui <abc@163.com>
2018-05-02 18:26:46 +0800

    添加去除字符串方法

--- utils.js
+++ utils.js
@@ -128,3 +128,28 @@ function getUrlAllParams(url) {
     }
     return _rs;
 }
+
+
+
+/**
+ * 去除空格
+ * @param  {str}
+ * @param  {type} 
+ *       type:  1:所有空格  2:前后空格  3:前空格 4:后空格
+ * @return {String}
+ */
+function trim(str, type) {
+    type = type || 1;
+    switch (type) {
+        case 1:
+            return str.replace(/\s+/g, '');
+        case 2:
+            return str.replace(/(^\s*)|(\s*$)/g, '');
+        case 3:
+            return str.replace(/(^\s*)/g, '');
+        case 4:
+            return str.replace(/(\s*$)/g, '');
+        default:
+            return str;
+    }
+}
