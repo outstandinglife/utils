eaa69a413f90a6b860b081d22278502eccd71115
lipenghui <abc@163.com>
2018-05-02 17:26:26 +0800

    添加查询url参数方法

--- utils.js
+++ utils.js
@@ -20,6 +20,7 @@ function css(ele, json) {
 }
 
 
+
 /** 操作localStorage
  *  Storage.get(key)    获取单个存储对象
  *  Storage.get()       获取所有存储对象
@@ -46,7 +47,8 @@ class Storage {
 
     set(key, val) {
         if (key && !this.isJSON(key)) this.storage.setItem(key, JSON.stringify(val));
-        else for (let a in key) this.set(a, key[a]);
+        else
+            for (let a in key) this.set(a, key[a]);
     }
 
     remove(key) {
@@ -62,7 +64,7 @@ class Storage {
     }
 
     isJSON(obj) {
-        return typeof obj === 'object' && Object.prototype.toString.call(obj).toLowerCase() === '[object object]' && ! obj.length;
+        return typeof obj === 'object' && Object.prototype.toString.call(obj).toLowerCase() === '[object object]' && !obj.length;
     }
 
     deserialize(value) {
@@ -82,4 +84,47 @@ class Storage {
             callback(key, this.get(key));
         }
     }
+}
+
+
+
+/** 数组排序
+ *@params   {arr}   排序的数组
+ *@params   {state} 排序的类型
+ *@author   lipenghui
+ *@return   arr 排序后的数组
+ **/
+function sort(arr, state = 1) {
+    return arr.sort((a, b) => {
+        switch (state) {
+            case 1:
+                return a - b;
+            case 2:
+                return b - a;
+            case 3:
+                return Math.random() - 0.5;
+            default:
+                return arr;
+        }
+    });
+}
+
+
+
+/** 获取url参数
+ *@params   {url}   查询的url链接
+ *@author   lipenghui
+ *@return   url对象
+ **/
+function getUrlAllParams(url) {
+    var url = url ? url : window.location.href;
+    var _pa = url.substring(url.indexOf('?') + 1);
+    var _arrS = _pa.split('&');
+    var _rs = {};
+    for (var i = 0, _len = _arrS.length; i < _len; i++) {
+        var pos = _arrS[i].indexOf('=');
+        if (pos == -1) continue;
+        _rs[_arrS[i].substring(0, pos)] = _arrS[i].substring(pos + 1);
+    }
+    return _rs;
 }
